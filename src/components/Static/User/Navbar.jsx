0bbfc767e5938dca8c28306e450d4729b24f6bbe
Thoobi <sydneydaniel76@gmail.com>
2025-01-16 17:44:34 +0100

    adds another changes to the proj

--- src/components/Static/User/Navbar.jsx
+++ src/components/Static/User/Navbar.jsx
@@ -1,6 +1,14 @@
+import { useEffect, useState } from 'react';
 import ktglogo from '../../../assets/ktg-logo.svg'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
+import { navcomponent } from '../../../lib/navbar'
+import { IoAddOutline } from "react-icons/io5";
+
 const Navbar = () => {
+  const [isActive, setIsActive] = useState("");
+  useEffect(()=>{
+
+  })
   return (
     <nav className="w-full bg-white h-14 border-b-[1px] border-b-black flex items-center px-10 absolute top-0 z-10 justify-between">
         <Link href={"/"} className='flex gap-2 items-center'>
@@ -10,17 +18,18 @@ const Navbar = () => {
           <h3 className='text-base font-medium'>KTG</h3>
         </Link>
       
-      <div>
-        <ul className='flex gap-5 text-sm'>
-          <Link className=''>SHOP</Link>
-          <Link>COLLECTIONS</Link>
-          <Link>ABOUT</Link>
-        </ul>
-      </div>
-      <div>
-        <ul className='text-sm'>
-          <li>CART</li>
-        </ul>
+      <NavLink className='flex flex-row gap-10'>
+        {navcomponent.map((items, index)=>(
+          <ul key={index} className={`${isActive === items.title ? "border-b-2 border-b-black" : ""} text-sm text-black p-1 `}>
+            <Link className='p-1' onClick={()=>{
+              setIsActive(items.title)
+            }}>{items.title}</Link>
+          </ul>
+        ))}
+      </NavLink>
+      <div className='text-sm flex items-center gap-3'>
+          <Link>CART</Link>
+          <span className='h-4 w-4 text-black font-medium cursor-pointer'><IoAddOutline className='h-full w-full' /></span>
       </div>
     </nav>
   )
---
+++ src/lib/navbar.js
@@ -0,0 +1,18 @@
+export const navcomponent = [
+    {
+        title: "SHOP",
+        content: "Hello world this is a character development session"
+    },
+    {
+        title: "COLLECTIONS",
+        content: "Hello world this is a character development session"
+    },
+    {
+        title: "STYLECHART",
+        content: "Hello world this is a character development session"
+    },
+    {
+        title: "ABOUT",
+        content: "Hello world this is a character development session"
+    },
+]
