3ec57d2e53b285ecf9be7a0379046da828374e4e
Thoobi <sydneydaniel76@gmail.com>
2025-07-09 18:15:13 +0100

    fix: Update path resolution in Vite config to ensure correct aliasing

--- vite.config.js
+++ vite.config.js
@@ -12,9 +12,9 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
-      "@components": path.resolve(__dirname, "src/components"),
-      "@assets": path.resolve(__dirname, "src/assets"),
+      "@": path.resolve(__dirname, "/src"),
+      "@components": path.resolve(__dirname, "/src/components"),
+      "@assets": path.resolve(__dirname, "/src/assets"),
     },
   },
 });
