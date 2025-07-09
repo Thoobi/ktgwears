661201a1a41c58a169421dd4d819a79b1c8984e1
Thoobi <sydneydaniel76@gmail.com>
2025-07-09 18:17:14 +0100

    fix: Simplify path resolution in Vite config by removing unnecessary imports

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-import path from "path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "/src"),
-      "@components": path.resolve(__dirname, "/src/components"),
-      "@assets": path.resolve(__dirname, "/src/assets"),
+      "@": "/src",
+      "@components": "/src/components",
+      "@assets": "/src/assets",
     },
   },
 });
