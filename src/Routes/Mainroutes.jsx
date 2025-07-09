5d0e77171edb94ee0ecb4246d1657efdc059425d
Thoobi <sydneydaniel76@gmail.com>
2025-07-09 19:20:59 +0100

    fix: Correct import path for WelcomeScreen in Mainroutes component

--- src/Routes/Mainroutes.jsx
+++ src/Routes/Mainroutes.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "@components/layout/userLayout";
-import { WelcomeScreen } from ".@/pages/userscreen/welcomeScreen";
+import { WelcomeScreen } from "@/pages/userscreen/welcomeScreen";
 import { Shop } from "@/pages/userscreen/shop";
 import { Stories } from "@/pages/userscreen/stories";
 import { Stylechart } from "@/pages/userscreen/stylechart";
